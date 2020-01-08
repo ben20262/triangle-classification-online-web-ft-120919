74ad77548896127452552ed26d8c2066f6744d8b
Benjamin Small <ben20262@aim.com>
2020-01-08 17:27:36 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -7,6 +7,23 @@ class Triangle
     @side_3 = side_3
   end
 
+  def kind
+    
+    if @side_1 == @side_2 && @side_1 == @side_3
+      return :equilateral
+    elsif @side_1 >= @side_2 + @side_3 || @side_2 >= @side_1 + @side_3 || @side_3 >= @side_1 + @side_2
+      begin
+        raise TriangleError
+      rescue TriangleError => error
+        puts error.message
+      end
+    elsif @side_1 == @side_2 || @side_1 == @side_3
+      return :isosceles
+    else
+      return :scalene
+    end
+  end
+
   class TriangleError < StandardError
   end
 
